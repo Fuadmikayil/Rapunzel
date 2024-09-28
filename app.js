03d59b49c77d325cbe2f5c083de292d055b9cc09
mikayilov2006@gmail.com <Fuadmikayil>
2024-09-28 11:08:05 +0400

    await

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-const cardsWrapper = document.querySelector(".cards-wrapper");
+const cardsWrapper = document.querySelector('.cards-wrapper');
 
-fetch('https://fakestoreapi.com/products/1')
-    .then(res => res.json())
-    .then((Praducts) => {
-        Praducts.map(item => {
-            const card = document.createElement("div")
-            card.classList.add("card")
-            card.innerHTML = ` <img src= ${item.image} alt=" "  
-    <h2> ${item.title.slice(0, 10)} ...</h2> 
-    <p> ${item.category} ...</p> 
-    <button> ${item.rating.rate} - Stconk: ${item.rating.count}</button> 
-      `
-            cardsWrapper.appendChild(card)
-        });
-        
-    });
+// Step 1 Fetch
+const response = await fetch('https://fakestoreapi.com/products');
+// Step 2 convert response to json
+const products = await response.json();
+// Step Show Data(products)
+products.map((item) => {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  card.innerHTML = `<img src=${item.image} alt="">
+                            <h2>${item.title.slice(0, 10)}...</h2>
+                            <p>${item.category}</p>
+                            <button>Rate:${item.rating.rate} -  Stock: ${
+    item.rating.count
+  }</button>`;
 
+  cardsWrapper.appendChild(card);
+});
